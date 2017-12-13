e3471c4d24d3d2fbe99ef359247c3088d1db0287
imdhi <1207487133@qq.com>
2017-12-13 21:42:46 +0800

    更新了persondetail活动

--- app/src/main/java/com/example/hp/iclass/ResetClassActivity.java
+++ app/src/main/java/com/example/hp/iclass/ResetClassActivity.java
@@ -1,13 +1,37 @@
 package com.example.hp.iclass;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.example.hp.iclass.CommonActivity.BeginningActivity.Login.LoginActivity;
+import com.example.hp.iclass.PersonCenter.PersonDetailActivity;
 
 public class ResetClassActivity extends AppCompatActivity {
-
+    private Toolbar tl_head;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reset_class);
+        tl_head = (Toolbar) findViewById(R.id.tl_head);
+        tl_head.setNavigationIcon(R.drawable.ic_back);
+        tl_head.setTitle("             更改班级");
+        tl_head.setTitleTextColor(Color.WHITE);
+        tl_head.setNavigationIcon(R.drawable.ic_back);
+        setSupportActionBar(tl_head);
+        tl_head.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                gotolast();
+            }
+        });
+    }
+    private void gotolast() {
+        Intent it = new Intent(this, PersonDetailActivity.class);
+        startActivity(it);
+        finish();
     }
 }
--- app/src/main/java/com/example/hp/iclass/ResetCollegeActivity.java
+++ app/src/main/java/com/example/hp/iclass/ResetCollegeActivity.java
@@ -1,13 +1,62 @@
 package com.example.hp.iclass;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.example.hp.iclass.PersonCenter.PersonDetailActivity;
 
 public class ResetCollegeActivity extends AppCompatActivity {
-
+private Toolbar tl_head;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reset_college);
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        tl_head = (Toolbar) findViewById(R.id.tl_head);
+        tl_head.setNavigationIcon(R.drawable.ic_back);
+        tl_head.setTitle("             更改学院");
+        tl_head.setTitleTextColor(Color.WHITE);
+        tl_head.setNavigationIcon(R.drawable.ic_back);
+        setSupportActionBar(tl_head);
+        tl_head.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                gotolast();
+            }
+        });
+        String[] mItems = getResources().getStringArray(R.array.languages);
+// 建立Adapter并且绑定数据源
+        ArrayAdapter<String> adapter=new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item, mItems);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//绑定 Adapter到控件
+        spinner .setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view,
+                                       int pos, long id) {
+
+                String[] languages = getResources().getStringArray(R.array.languages);
+                Toast.makeText(ResetCollegeActivity.this, "你点击的是:"+languages[pos], Toast.LENGTH_SHORT).show();
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // Another interface callback
+            }
+        });
+    }
+    private void gotolast() {
+        Intent it = new Intent(this, PersonDetailActivity.class);
+        startActivity(it);
+        finish();
+
     }
 }
