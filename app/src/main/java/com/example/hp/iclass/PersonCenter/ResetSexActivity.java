548469996d83ca6e680b983ff744650ea52f983d
imdhi <1207487133@qq.com>
2017-12-14 11:19:13 +0800

    改性别变成spinner 改密码前面加字了

--- app/src/main/java/com/example/hp/iclass/PersonCenter/ResetSexActivity.java
+++ app/src/main/java/com/example/hp/iclass/PersonCenter/ResetSexActivity.java
@@ -6,6 +6,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.example.hp.iclass.PersonCenter.PersonDetailActivity;
 import com.example.hp.iclass.R;
@@ -16,6 +20,7 @@ public class ResetSexActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reset_sex);
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
         tl_head = (Toolbar) findViewById(R.id.tl_head);
         tl_head.setNavigationIcon(R.drawable.ic_back);
         tl_head.setTitle("             更改性别");
@@ -26,6 +31,26 @@ public class ResetSexActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 gotolast();
+            }
+        });
+        String[] mItems = getResources().getStringArray(R.array.sex);
+// 建立Adapter并且绑定数据源
+        ArrayAdapter<String> adapter=new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item, mItems);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//绑定 Adapter到控件
+        spinner .setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view,
+                                       int pos, long id) {
+
+                String[] languages = getResources().getStringArray(R.array.sex);
+                Toast.makeText(ResetSexActivity.this, "你点击的是:"+languages[pos], Toast.LENGTH_SHORT).show();
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // Another interface callback
             }
         });
     }
